86e5241680c677386d620c0d019b09402a6884ce
unknown <ismail.ouali@ump.ac.ma>
2024-05-14 12:37:36 +0000

    trying to fix the error (serializer)

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import TaskListCreate, TaskDetail
 
 urlpatterns = [
+    path('', views.index, name = 'index'),
     path('tasks/', TaskListCreate.as_view(), name='task-list-create'),
     path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .models import Task
-from .serializers import TaskSerializer
+from ..serializers import TaskSerializer
 
 class TaskListCreate(generics.ListCreateAPIView):
     queryset = Task.objects.all()
